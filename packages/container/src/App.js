3fc613bddd6bbb6f155325837d5e8926e2275216
ivyas <ivyas@comlinkdata.com>
2024-10-28 18:48:50 +0530

    add invalidation script-2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return (
-        <div><p>Container here yo!!</p>
+        <div><p>Container here yozzz!!</p>
             <MarketingApp/>
         </div>
         
